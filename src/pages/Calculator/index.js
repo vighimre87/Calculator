37497ed35da7a0deded39e8daccdd3a9f10d210c
Mingzhao91 <edwardwhite7@hotmail.com>
2023-03-22 20:48:35 +0000

    create pages

---
+++ src/pages/Calculator/index.js
@@ -0,0 +1,9 @@
+// import libraries
+import React from "react";
+
+// import styling
+import "./style.css";
+
+function Calculator() {}
+
+export default Calculator;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+// import libraries
+import React from "react";
+
+// import styling
+import "./style.css";
+
+function Home() {}
+
+export default Home;
