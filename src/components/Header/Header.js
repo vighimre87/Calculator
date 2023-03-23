0264c2c36b52aacf7fdce347ad52446deda5573b
Imre Vigh <vighimre87@gmail.com>
2023-03-23 19:16:16 +0000

    created header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {AppBar, Toolbar, Tab, Tabs, Typography} from '@mui/material';
+import CalculateIcon from '@mui/icons-material/Calculate';
+
+
+function Header() {
+    return (
+        <header>
+          <AppBar sx={{background: '#3F91BF'}}>
+            <Toolbar>
+              <CalculateIcon />
+              <Tabs sx={{marginLeft: 'auto'}} textColor='inherit'>
+                <Tab label='Home'></Tab>
+                <Tab label='Calculator'></Tab>
+                <Tab label='Currency'></Tab>
+                <Tab label='Units'></Tab>
+              </Tabs>
+            </Toolbar>
+          </AppBar>
+        </header>
+    );
+}
+
+export default Header;
