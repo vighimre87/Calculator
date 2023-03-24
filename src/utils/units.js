086ece61c9ee6dcb185569405e4764467d2ad11e
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-03-24 21:15:41 +0000

    create EXCHANGE_OPTIONS constant

---
+++ src/utils/units.js
@@ -0,0 +1,38 @@
+export const EXCHANGE_OPTIONS = [
+  {
+    label: "Area",
+    value: "area"
+  },
+  {
+    label: "Frequency",
+    value: "frequency"
+  },
+  {
+    label: "Length",
+    value: "length"
+  },
+  {
+    label: "Mass",
+    value: "mass"
+  },
+  {
+    label: "Power",
+    value: "power"
+  },
+  {
+    label: "Speed",
+    value: "speed"
+  },
+  {
+    label: "Temperature",
+    value: "temperature"
+  },
+  {
+    label: "Time",
+    value: "time"
+  },
+  {
+    label: "Volumn",
+    value: "volumn"
+  }
+];
