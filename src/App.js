11eb39bc5b6f4ac41285a812379750851a7d78e9
xiaozhao1111 <zhaoxiao20071106@gmail.com>
2023-03-24 07:28:44 +0000

    set full width for footer

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <Router>
       <div>
-        {/* <Header /> */}
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="calculator" element={<Calculator />} />
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import './Footer.css'
 
 function Footer() {
     return (
@@ -9,9 +10,10 @@ function Footer() {
             <Box component="footer"
                 sx={{
                     py: 2, 
-                    backgroundColor: '#3F91BF'
+                    backgroundColor: '#3F91BF',
+                    marginTop: 'auto',
                 }}>
-                <Container maxWidth="sm">
+                <Container maxWidth="sm" sx={{marginTop: 'auto', marginBottom: 'auto'}}>
                     <Typography variant="body2" color="#f5f5f5" textAlign='center'>
                         {'Copyright © Pixel Pionners '}
                         {new Date().getFullYear()}
