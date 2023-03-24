e2d848085440830b5dbffc3c2e2fdfa2453e7c66
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-03-24 22:21:25 +0000

    create unit dropdowns with state management

--- src/pages/Units/index.js
+++ src/pages/Units/index.js
@@ -1,35 +1,74 @@
 // import libraries
 import React, { useState, useEffect } from "react";
+import convert from "convert-units";
 
 // import components
 import UnitDropdown from "../../components/UnitDropdown/UnitDropdown";
 
 // import constants
-import { EXCHANGE_OPTIONS } from "../../utils/units";
+import { UNITS } from "../../utils/units";
 
 // import styling
 import "./style.css";
 
 function Units() {
+  // exchange option state management
   const [exchangeOption, setExchangeOption] = useState(
-    EXCHANGE_OPTIONS[0].value
+    UNITS.EXCHANGE_OPTIONS[0].value
   );
-
-  // listen to the changes of exchangeOption
   useEffect(() => {
     console.log("exchangeOption is changed.....", exchangeOption);
+    const optionName = UNITS[`${exchangeOption.toUpperCase()}_UNITS`];
+    setUnitOptions(optionName);
+    setLeftUnitOption("");
+    setRightUnitOption("");
   }, [exchangeOption]);
 
+  // left dropdown state management
+  const [leftUnitOption, setLeftUnitOption] = useState("");
+
+  // right dropdown state management
+  const [rightUnitOption, setRightUnitOption] = useState("");
+
+  // unit options state management
+  const [unitOptions, setUnitOptions] = useState();
+
+  // mi2
+  // console.log(convert().describe("mi2"));
+
   return (
-    <UnitDropdown
-      id="exchange"
-      label="Options"
-      selectedValue={exchangeOption}
-      options={EXCHANGE_OPTIONS}
-      handleChange={(event) => {
-        setExchangeOption(event.target.value);
-      }}
-    />
+    <div>
+      <UnitDropdown
+        // exchange option dropdown
+        id="exchange"
+        label="Options"
+        selectedValue={exchangeOption}
+        options={UNITS.EXCHANGE_OPTIONS}
+        handleChange={(event) => {
+          setExchangeOption(event.target.value);
+        }}
+      />
+      <UnitDropdown
+        // left dropdown
+        id="left-dropdown"
+        label="Select Unit"
+        selectedValue={leftUnitOption}
+        options={unitOptions}
+        handleChange={(event) => {
+          setLeftUnitOption(event.target.value);
+        }}
+      />
+      <UnitDropdown
+        // right dropdown
+        id="right-dropdown"
+        label="Select Unit"
+        selectedValue={rightUnitOption}
+        options={unitOptions}
+        handleChange={(event) => {
+          setRightUnitOption(event.target.value);
+        }}
+      />
+    </div>
   );
 }
 
