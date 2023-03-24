b35e7b29a425774868c1ef5689a8b99b7275688a
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-03-24 21:15:58 +0000

    setup dropdown for exchange option

--- src/pages/Units/index.js
+++ src/pages/Units/index.js
@@ -1,9 +1,36 @@
 // import libraries
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// import components
+import UnitDropdown from "../../components/UnitDropdown/UnitDropdown";
+
+// import constants
+import { EXCHANGE_OPTIONS } from "../../utils/units";
 
 // import styling
 import "./style.css";
 
-function Units() {}
+function Units() {
+  const [exchangeOption, setExchangeOption] = useState(
+    EXCHANGE_OPTIONS[0].value
+  );
+
+  // listen to the changes of exchangeOption
+  useEffect(() => {
+    console.log("exchangeOption is changed.....", exchangeOption);
+  }, [exchangeOption]);
+
+  return (
+    <UnitDropdown
+      id="exchange"
+      label="Options"
+      selectedValue={exchangeOption}
+      options={EXCHANGE_OPTIONS}
+      handleChange={(event) => {
+        setExchangeOption(event.target.value);
+      }}
+    />
+  );
+}
 
 export default Units;
