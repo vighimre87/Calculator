4475629fb4c5441939e790a81fe46b8df1f85760
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-03-24 22:19:49 +0000

    add AREA_UNITS and MASS_Units constants

--- src/utils/units.js
+++ src/utils/units.js
@@ -1,38 +1,112 @@
-export const EXCHANGE_OPTIONS = [
-  {
-    label: "Area",
-    value: "area"
-  },
-  {
-    label: "Frequency",
-    value: "frequency"
-  },
-  {
-    label: "Length",
-    value: "length"
-  },
-  {
-    label: "Mass",
-    value: "mass"
-  },
-  {
-    label: "Power",
-    value: "power"
-  },
-  {
-    label: "Speed",
-    value: "speed"
-  },
-  {
-    label: "Temperature",
-    value: "temperature"
-  },
-  {
-    label: "Time",
-    value: "time"
-  },
-  {
-    label: "Volumn",
-    value: "volumn"
-  }
-];
+export const UNITS = {
+  EXCHANGE_OPTIONS: [
+    {
+      label: "Area",
+      value: "area"
+    },
+    {
+      label: "Frequency",
+      value: "frequency"
+    },
+    {
+      label: "Length",
+      value: "length"
+    },
+    {
+      label: "Mass",
+      value: "mass"
+    },
+    {
+      label: "Power",
+      value: "power"
+    },
+    {
+      label: "Speed",
+      value: "speed"
+    },
+    {
+      label: "Temperature",
+      value: "temperature"
+    },
+    {
+      label: "Time",
+      value: "time"
+    },
+    {
+      label: "Volumn",
+      value: "volumn"
+    }
+  ],
+  AREA_UNITS: [
+    {
+      label: "Square Millimeter - mm2",
+      value: "mm2"
+    },
+    {
+      label: "Square Centimeter - cm2",
+      value: "cm2"
+    },
+    {
+      label: "Square Meter - m2",
+      value: "m2"
+    },
+    {
+      label: "Hectare - ha",
+      value: "ha"
+    },
+    {
+      label: "Square Kilometer - km2",
+      value: "km2"
+    },
+    {
+      label: "Square Inches - in2",
+      value: "in2"
+    },
+    {
+      label: "Square Foot - ft2",
+      value: "ft2"
+    },
+    {
+      label: "Acre - ac",
+      value: "ac"
+    },
+    {
+      label: "Square Mile - mi2",
+      value: "mi2"
+    }
+  ],
+  MASS_UNITS: [
+    {
+      label: "Microgram - mcg",
+      value: "mcg"
+    },
+    {
+      label: "Milligram - mg",
+      value: "mg"
+    },
+    {
+      label: "Gram - g",
+      value: "g"
+    },
+    {
+      label: "Kilogram - kg",
+      value: "kg"
+    },
+    {
+      label: "Ounce - oz",
+      value: "oz"
+    },
+    {
+      label: "Pound - lb",
+      value: "lb"
+    },
+    {
+      label: "Metric Tonne - mt",
+      value: "mt"
+    },
+    {
+      label: "Ton - t",
+      value: "t"
+    }
+  ]
+};
