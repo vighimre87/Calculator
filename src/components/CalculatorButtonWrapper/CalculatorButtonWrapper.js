41a23e08badb3f44ab0f0142ee3a9c8db4815f18
Imre Vigh <vighimre87@gmail.com>
2023-03-24 19:09:48 +0000

    made wrapper for buttons

---
+++ src/components/CalculatorButtonWrapper/CalculatorButtonWrapper.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CalculatorButtonWrapper({children}) {
+    return <div className='buttonContainer'>{children}</div>;
+}
+
+export default CalculatorButtonWrapper;
