b3a382de7e377eb3117a1b6bfe99ad1ab3441f75
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-03-25 16:53:34 +0000

    remove redundant React import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import libraries
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import React from "react";
 
 // import pages
 import Home from "./pages/Home/index";
