5d890c5e9572c3dc346140048eb6a307eed99f60
xiaozhao1111 <zhaoxiao20071106@gmail.com>
2023-03-25 12:29:09 +0000

    install axios package

--- src/components/ConverterContainer/ConverterContainer.js
+++ src/components/ConverterContainer/ConverterContainer.js
@@ -1,18 +1,25 @@
 import * as React from 'react';
 import CurrencyDropdown from '../CurrencyDropdown/CurrencyDropdown';
 
-
+import { useState, useEffect } from 'react';
 
 function ConverterContainer() {
 
-  
+  const [amount1, setAmount1] = useState(1);
+  const [amount2, setAmount2] = useState(1);
+  const [currency1, setCurrency1] = useState('USD');
+  const [currency2, setCurrency2] = useState('USD');
+
+
+  useEffect(() => {}, 
+  [])
 
   return (
     <div>
       <p>From </p>
-      <CurrencyDropdown amount={1} currency='USD' />
+      <CurrencyDropdown amount={amount1} currency={currency1} />
       <p>To</p>
-      <CurrencyDropdown amount={1} currency='USD' />
+      <CurrencyDropdown amount={amount2} currency={currency2} />
     </div>
   );
 }
