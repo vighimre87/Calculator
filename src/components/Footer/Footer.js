55bf314040ab166ae7c2582e2e1336ba6d24b8b0
xiaozhao1111 <zhaoxiao20071106@gmail.com>
2023-03-28 19:24:24 +0100

    delete the footer css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import './Footer.css'
 
 function Footer() {
     return (
